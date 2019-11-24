95559e8d121123a4e587093b44819d9c1d364953
Veer275# <raghuveer.mh@cognitiveclouds.com>
2019-11-24 19:22:11 +0530

    Updates

---
+++ precache-manifest.8deac285796d45e7a1dac482f064a26d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee64e9a78b171efd48a2a5bfbe8cc25",
+    "url": "/my-git/index.html"
+  },
+  {
+    "revision": "84a027545740e3ad4ee0",
+    "url": "/my-git/static/css/main.0cccc489.chunk.css"
+  },
+  {
+    "revision": "bbda373919275a713fef",
+    "url": "/my-git/static/js/2.732a9f86.chunk.js"
+  },
+  {
+    "revision": "84a027545740e3ad4ee0",
+    "url": "/my-git/static/js/main.bcea6f93.chunk.js"
+  },
+  {
+    "revision": "b69155280b6686cb2b7d",
+    "url": "/my-git/static/js/runtime~main.64fa828d.js"
+  }
+]);
